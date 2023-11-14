8119a6d10b548a394a30ae007ef5f71e1b94e03e
mqorra <qorra.mouhcine1@gmail.com>
2023-11-14 09:54:59 +0100

    Configure Karate pom.xml

--- cms-v0/src/test/java/ma/sqli/peps/TestRunner.java
+++ cms-v0/src/test/java/ma/sqli/peps/TestRunner.java
@@ -1,8 +1,22 @@
 package ma.sqli.peps;
 
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
 
-@RunWith(Karate.class)
+import com.intuit.karate.junit5.Karate;
+import org.junit.jupiter.api.BeforeAll;
+
+
 public class TestRunner {
+
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.setProperty("karate.env", "localhost");
+    }
+
+    @Karate.Test
+    Karate HappyOrderSave() {
+        return Karate.run("test/HappyOrderTest").relativeTo(getClass());
+    }
+
+
 }
